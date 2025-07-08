f3487792e9fa4aa98cbd3363a1b46662b48b0ba5
Naphat Permpredanun <68365175+NaphatPRM@users.noreply.github.com>
2025-07-08 23:20:38 +0700

    Update elgamal.py
    Solving the issue of slow run due to nuance of modulo and power

--- elgamal.py
+++ elgamal.py
@@ -6,20 +6,21 @@ from params import g
 def keygen():
     q = (p - 1) / 2
     sk = random.randint(1, q)
-    pk = (g ** sk) % p
+    pk = pow(g, sk, p)
     return pk,sk
 
 
 def encrypt(pk,m):
     q = (p - 1) / 2
     r = random.randint(1, q)
-    c1 = (g ** r) % p
-    c2 = ((pk ** r) * m) % p
+    c1 = pow(g, r, p)
+    c2 = (pow(pk, r, p) * m) % p
     return [c1,c2]
 
 
 def decrypt(sk,c):
-    val_below = modular_inverse(c[0] ** sk, -1, p)
+    first = pow(c[0], sk, p)
+    val_below = modular_inverse(first, p)
     m = (c[1] * val_below) % p
     return m
 
